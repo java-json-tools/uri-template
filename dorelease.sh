ab49a94369ccee9e91be354e91140d6e0a9e55bc
capstan <nherring@google.com>
2019-09-09 01:00:20 +0200

    Update gradle, fix groupid.

--- dorelease.sh
+++ dorelease.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-./gradlew --recompile-scripts clean test uploadArchives
+# Release instructions
+# 1. Follow the gpg setup instructions: http://central.sonatype.org/pages/working-with-pgp-signatures.html
+#   a. gpg2 --export-secret-keys > ~/.gnupg/secring.gpg
+#   b. gpg2 --list-keys --keyid-format short
+# 2. In ~/.gradle add:
+# sonatypeUsername=your sonatype user name
+# sonatypePassword=password in vault for oss sonatype<ensure newline>
+#
+# signing.keyId=generated key in short format
+# signing.password=password for gpg key
+# signing.secretKeyRingFile=/Users/username/.gnupg/secring.gpg
+#
+# 3. ./dorelease.sh
+# 4. Go to UI at: https://oss.sonatype.org
+# 5. Login as sonatypeUsername
+# 6. Follow the instructions here: http://central.sonatype.org/pages/releasing-the-deployment.html
+
+./gradlew clean test uploadArchives
 
